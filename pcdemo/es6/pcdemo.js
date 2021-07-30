2773aca00f32a8c8bb905c87efdb12dc4167f024
joma <joma@twilio.com>
2021-07-30 14:02:55 -0700

    Senders

--- pcdemo/es6/pcdemo.js
+++ pcdemo/es6/pcdemo.js
@@ -197,13 +197,13 @@ function managePC({ parentDiv, myName }) {
       thisPC.addEventListener("icegatheringstatechange", (event) => {
         if (thisPC.iceGatheringState === "complete") {
           const senders = thisPC.getSenders();
-
-          senders.forEach((sender) => {
-            if (sender.track.kind === "video") {
-              codecList = sender.getParameters().codecs;
-              return;
-            }
-          });
+          console.log('SENDERS : ', senders);
+          // senders.forEach((sender) => {
+          //   if (sender.track.kind === "video") {
+          //     codecList = sender.getParameters().codecs;
+          //     return;
+          //   }
+          // });
         }
         console.log(`${myName}: icegatheringstatechange CODEC LIST`, codecList);
         codecList = null;
